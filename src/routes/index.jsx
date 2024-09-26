d31d28e6513346ce1ebb3fa196f0b802759cd0e3
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-09-25 21:08:09 -0400

    adicione motion

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,25 +1,55 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import { motion } from 'framer-motion';  // Importando o Framer Motion
 import { Login } from '../containers/Login';
 import { Register } from '../containers/Register';
 import { Home } from '../containers/Home';
 import { Menu } from '../containers/Menu';
 
+const AnimatedPage = ({ children }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -100 }}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
 export const router = createBrowserRouter([
   {
     path: '/login',
-    element: <Login />,
+    element: (
+      <AnimatedPage>
+        <Login />
+      </AnimatedPage>
+    ),
   },
   {
     path: '/cadastro',
-    element: <Register />,
+    element: (
+      <AnimatedPage>
+        <Register />
+      </AnimatedPage>
+    ),
   },
   {
     path: '/',
-    element: <Home />
+    element: (
+      <AnimatedPage>
+        <Home />
+      </AnimatedPage>
+    ),
   },
   {
     path: '/cardapio',
-    element: <Menu />
+    element: (
+      <AnimatedPage>
+        <Menu />
+      </AnimatedPage>
+    ),
   },
 ]);
