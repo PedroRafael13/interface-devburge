edce6526c4dce75190bd94793156b7750df1d563
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-10-03 20:38:53 -0400

    resolvendo problemas de importação

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import { motion } from 'framer-motion';  // Importando o Framer Motion
 import { Login } from '../containers/Login';
 import { Register } from '../containers/Register';
 import { Home } from '../containers/Home';
@@ -8,20 +7,7 @@ import { Menu } from '../containers/Menu';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import { Cart } from '../components/Cart';
-
-
-const AnimatedPage = ({ children }) => {
-  return (
-    <motion.div
-      initial={{ opacity: 0, x: 100 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -100 }}
-      transition={{ duration: 0.5 }}
-    >
-      {children}
-    </motion.div>
-  );
-};
+import { AnimatedPage } from '../components/Animation';
 
 export const router = createBrowserRouter([
   {
